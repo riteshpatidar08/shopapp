db1aa91d92b52e7ca05436a5c9809e049793ee5c
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-07-12 09:34:31 +0530

    m

--- src/components/GridSection.jsx
+++ src/components/GridSection.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 function GridSection() {
   return (
-    <div>
-      <h1></h1>
+    <div className="max-w-7xl mx-auto mt-14 p-2">
+      <h1 className="text-center leading-wide text-2xl mb-4 font-medium">
+        STYLISH PICKS FOR EVERY OCCASION
+      </h1>
+      {/* grid section */}
+      <div className="grid grid-cols-4 grid-rows-2">
+        {/* //large image */}
+        <div>
+          <div></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default GridSection
+export default GridSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import TopStrip from '../components/TopStrip';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import GridSection from '../components/GridSection';
 function Home() {
   return (
     <div>
       <TopStrip />
       <Navbar />
       <Hero />
+      <GridSection/>
       <div className="px-28 py-7">
         <Banner />
       </div>
