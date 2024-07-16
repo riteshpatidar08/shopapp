c4cd650d37cabd847368d134c4f047a051177c37
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-07-16 11:12:33 +0530

    udpatedCode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState} from 'react';
 import logo from './../assets/logo1.png';
 import { HiOutlineShoppingBag } from 'react-icons/hi2';
 import { IoLogInOutline } from 'react-icons/io5';
@@ -7,14 +7,35 @@ import { GoHeart } from 'react-icons/go';
 import NavItems from './NavItems';
 import { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import all_products from '../assets/all_product';
 function Navbar() {
   const inputRef = useRef(null);
   const { items, totalQuantity } = useSelector((state) => state.cart);
-  console.log(totalQuantity);
+ const suggestionRef = useRef(null)
+ const [searchTerm  , setSearchTerm ] = useState('') ;
+const [suggestionsItems , setSuggestionsItems] = useState([])
+
   useEffect(() => {
     inputRef.current.focus();
   }, []);
 
+  // useEffect(()=>{
+  //   if(suggestionRef.current && !suggestionRef.conta)
+  // })
+
+console.log(suggestionsItems)
+
+const handleChange = (event) => {
+   const value = event.target.value
+setSearchTerm(value)
+
+ if(value.length > 0){
+  const filteredProduct = all_products.filter((product)=>{
+    return product.name.toLowerCase().includes(value.toLowerCase())
+  })
+ setSuggestionsItems(filteredProduct)
+ }
+}
   return (
     <nav className="p-4 border-b border-gray-200 sticky top-0 bg-white z-10">
       <div>
@@ -24,13 +45,28 @@ function Navbar() {
             <img className="h-8" src={logo}></img>
             <h1 className="text-xl tracking-wide font-semibold">Shopping</h1>
           </div>
-          <div className="w-96">
+          <div className="w-96 mt-2 relative">
             <input
               ref={inputRef}
               type="text"
               placeholder="Search"
+              onChange={handleChange}
               className="w-full p-2 font-normal bg-white focu border border-gray-400 rounded-none"
             />
+            {
+              suggestionsItems.length > 0 ? (
+                <ul ref={suggestionRef} className='absolute z-10  bg-white max-h-60 overflow-y-auto border mt-2 border-gray-200 w-full'>
+                 {suggestionsItems.map((item)=>(
+                  <li className='my-2 hover:bg-gray-100 p-2'>
+                    <Link className="flex  gap-4 items-center">
+                    <img className='w-6 h-6 object-cover' src={item.image}/>
+                    <span className='text-[14px]'>{item.name}</span>
+                    </Link>
+                  </li>
+                 ))}
+                </ul>
+              ) : ''
+            }
           </div>
           <div className="flex item-center gap-14">
             <Link to="">
